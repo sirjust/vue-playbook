9244076dc359294fbb5f0cfe0dd328893567cc57
JustinMoore <justin.moore@perspecta.com>
2020-06-14 23:07:19 -0400

    Learn bindings, templates, and directives

---
+++ vue.js
@@ -0,0 +1,13 @@
+var growler = new Vue({
+    el: '#growler',
+    data: {
+        appName: '<a href="/">Growler</a>',
+        accentColor: {
+            color: '#FF6A00'
+        },
+        headers: {
+            fontFamily: 'Verdana',
+            margin: 0
+        }
+    }
+});
